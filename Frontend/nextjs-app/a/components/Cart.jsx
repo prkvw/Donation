ec1226d2aadffdd228dfd6cb941131cf70679353
prkvw <neeqash@gmail.com>
2023-11-08 17:15:39 +0000

    navbar components

---
+++ Frontend/nextjs-app/a/components/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Header} from '../components';
+import {Cart} from 'react-icons';
+
+const Cart = () => {
+  return (
+    <div className= "m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="App" title="Cart" />
+     </div>
+  )
+}
+
+export default Cart
