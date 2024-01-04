89b0b3c9befce15c2983304463dfe09d122006cc
prkvw <neeqash@gmail.com>
2024-01-04 23:07:24 +0000

    index update

--- Frontend/nextjs-app/index.js
+++ Frontend/nextjs-app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-import App from './App';
+import App from './pages/App';
 
 import Link from 'next/link';
 import { ContextProvider } from './contexts/ContextProvider';
